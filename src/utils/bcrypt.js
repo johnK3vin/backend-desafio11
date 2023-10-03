4721725d28cc4521344497e1484768b372e91719
johnkevin <johnkevinvegaolivo@gmail.com>
2023-10-03 00:16:45 -0300

    github configurado correctamente

--- src/utils/bcrypt.js
+++ src/utils/bcrypt.js
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt';
 
-export const createHash = (password) => bcrypt.hashSync(password, bcrypt.genSaltSync(parseInt(process.env.SALT)))
+export const createHash = (password) => bcrypt.hashSync(password, bcrypt.genSaltSync(10))
 
 export const validatePassword = (passwordSend, passwordBDD) => bcrypt.compareSync(passwordSend, passwordBDD)
